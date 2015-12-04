79b0b52c116e10186ce8576cd8b489307f82ce95
unoboros <unoboros@ubuntu.(none)>
2015-12-03 19:29:09 -0500

    BGD ready for test

--- src/mobius/LR_BGD.java
+++ src/mobius/LR_BGD.java
@@ -1,13 +1,13 @@
 package mobius;
 
 import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.URI;
+import java.net.URISyntaxException;
 
 import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.FSDataInputStream;
 import org.apache.hadoop.fs.FSDataOutputStream;
 import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.Path;
@@ -21,7 +21,6 @@ import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 import org.apache.hadoop.util.GenericOptionsParser;
 
-@SuppressWarnings("deprecation")
 public class LR_BGD {
 
 	public enum MSECounter {
@@ -35,34 +34,31 @@ public class LR_BGD {
 
 	public static class MSEMapper extends Mapper<Object, Text, Text, FloatWritable> {
 		
-		public String[] weights;
+		public float[] weights;
 
 		@Override
 		public void setup(Context context) throws IOException, InterruptedException {
 			
 			try {
-				URI[] files = context.getCacheFiles();
-				if (files == null || files.length == 0) {
-					throw new RuntimeException("File information is not set in DistributedCache");
-				}
-				// Read all files in the DistributedCache
-				for (URI u : files) {
-					Path p = new Path(u);
-					BufferedReader rdr = new BufferedReader(
-							new InputStreamReader(new FileInputStream(new File(p.toString()))));
-
-					String line;
-					// For each record in the user file
-					if ((line = rdr.readLine()) != null) {
-						// read in weights here
-						weights = line.split(",");
-						assert(weights.length == DIM);
+				FileSystem fs = FileSystem.get(new URI("s3://finalapp"),context.getConfiguration());
+				FSDataInputStream in = fs.open(new Path("s3://finalapp/weight/data.txt"));
+				BufferedReader rdr = new BufferedReader(new InputStreamReader(in));
+				String line;
+				if ((line = rdr.readLine()) != null) {
+					// read in weights here
+					String[] ws = line.split(",");
+					assert(ws.length == DIM);
+					for(int i=0;i<DIM;i++){
+						weights[i] = Float.parseFloat(ws[i]);
 					}
-					
-					rdr.close();
 				}
+				rdr.close();
+				in.close();
+				
 			} catch (IOException e) {
 				throw new RuntimeException(e);
+			} catch (URISyntaxException e) {
+				e.printStackTrace();
 			}
 		}
 
@@ -71,8 +67,12 @@ public class LR_BGD {
 			// partial summation
 			String strs[] = value.toString().split(",");
 			assert(strs.length == DIM + 1);
-			float y = Float.parseFloat(strs[DIM]);
-			float err = (logistic(weights, strs)-y);
+			float x[] = new float[DIM+1];
+			for(int i=0;i<DIM+1;i++){
+				x[i] = Float.parseFloat(strs[i]);
+			}
+			float y = x[DIM];
+			float err = (logistic(weights, x)-y);
 			context.write(new Text("_"), new FloatWritable(err*err));
 		}
 	}
@@ -100,57 +100,53 @@ public class LR_BGD {
 	}
 
 	public static class BGDMapper extends Mapper<Object, Text, Text, Text> {
-		public String[] weights;
-		public String[] partials = new String[DIM];
+		public float[] weights;
+		public float[] partials = new float[DIM];
 
 		@Override
 		public void setup(Context context) throws IOException, InterruptedException {
 			
-			for(int i=0;i<DIM;i++){
-				partials[i] = "0";
-			}
-			
 			try {
-				URI[] files = context.getCacheFiles();
-				if (files == null || files.length == 0) {
-					throw new RuntimeException("File information is not set in DistributedCache");
-				}
-				// Read all files in the DistributedCache
-				for (URI u : files) {
-					Path p = new Path(u);
-					BufferedReader rdr = new BufferedReader(
-							new InputStreamReader(new FileInputStream(new File(p.toString()))));
-
-					String line;
-					// For each record in the user file
-					if ((line = rdr.readLine()) != null) {
-						// read in weights here
-						weights = line.split(",");
-						assert(weights.length == DIM);
+				FileSystem fs = FileSystem.get(new URI("s3://finalapp"),context.getConfiguration());
+				FSDataInputStream in = fs.open(new Path("s3://finalapp/weight/data.txt"));
+				BufferedReader rdr = new BufferedReader(new InputStreamReader(in));
+				String line;
+				if ((line = rdr.readLine()) != null) {
+					// read in weights here
+					String[] ws = line.split(",");
+					assert(ws.length == DIM);
+					for(int i=0;i<DIM;i++){
+						weights[i] = Float.parseFloat(ws[i]);
+						partials[i] = 0;
 					}
-					
-					rdr.close();
 				}
+				rdr.close();
+				in.close();
+				
 			} catch (IOException e) {
 				throw new RuntimeException(e);
+			} catch (URISyntaxException e) {
+				e.printStackTrace();
 			}
 		}
 
 		public void map(Object line, Text value, Context context) throws IOException, InterruptedException {
 			String strs[] = value.toString().split(",");
 			assert(strs.length == DIM + 1);
+			float x[] = new float[DIM+1];
+			for(int i=0;i<DIM+1;i++){
+				x[i] = Float.parseFloat(strs[i]);
+			}
+			float y = x[DIM];
 			for(int j=0; j < DIM; j++){
-				float xj = Float.parseFloat(strs[j]);
-				float y = Float.parseFloat(strs[DIM]);
-				float partial = (logistic(weights, strs)-y)*xj + Float.parseFloat(partials[j]);
-				partials[j] = ""+partial;
+				partials[j] += (logistic(weights, x)-y)*x[j];
 			}
 		}
 		
 		protected void cleanup(Context context) throws IOException, InterruptedException {
 			StringBuilder sb = new StringBuilder();
-			for(String s: partials){
-				sb.append(s+",");
+			for(float p: partials){
+				sb.append(""+p+",");
 			}
 			context.write(new Text("_"), new Text(sb.toString()));
 		}
@@ -161,44 +157,34 @@ public class LR_BGD {
 		FileSystem fs;
 		FSDataOutputStream out;
 		Path weightPath;
-		public String[] weights;
-		public String[] partials = new String[DIM];
+		public float[] weights;
+		public float[] partials = new float[DIM];
+		StringBuilder newWeights = new StringBuilder();
 
 		@Override
 		public void setup(Context context) throws IOException, InterruptedException {
-			
-			for(int i=0;i<DIM;i++){
-				partials[i] = "0";
-			}
 
-			fs = FileSystem.get(context.getConfiguration());
-			weightPath = new Path("weight/data.txt");
-			if (fs.exists(weightPath)) {
-				fs.delete(weightPath);
-			}
-			out = fs.create(weightPath);
 			try {
-				URI[] files = context.getCacheFiles();
-				if (files == null || files.length == 0) {
-					throw new RuntimeException("File information is not set in DistributedCache");
-				}
-				// Read all files in the DistributedCache
-				for (URI u : files) {
-					Path p = new Path(u);
-					BufferedReader rdr = new BufferedReader(
-							new InputStreamReader(new FileInputStream(new File(p.toString()))));
-
-					String line;
-					// For each record in the user file
-					if ((line = rdr.readLine()) != null) {
-						weights = line.split(",");
-						assert(weights.length == DIM);
+				fs = FileSystem.get(new URI("s3://finalapp"),context.getConfiguration());
+				FSDataInputStream in = fs.open(new Path("s3://finalapp/weight/data.txt"));
+				BufferedReader rdr = new BufferedReader(new InputStreamReader(in));
+				String line;
+				// For each record in the user file
+				if ((line = rdr.readLine()) != null) {
+					String[] ws = line.split(",");
+					assert(ws.length == DIM);
+					for(int i=0;i<DIM;i++){
+						weights[i] = Float.parseFloat(ws[i]);
+						partials[i] = 0;
 					}
-					
-					rdr.close();
 				}
+				rdr.close();
+				in.close();
+
 			} catch (IOException e) {
 				throw new RuntimeException(e);
+			} catch (URISyntaxException e) {
+				e.printStackTrace();
 			}
 		}
 
@@ -210,20 +196,20 @@ public class LR_BGD {
 				String[] contrib = value.toString().split(",");
 				assert(contrib.length == DIM);
 				for(int i=0;i<DIM;i++){
-					partials[i] = ""+(Float.parseFloat(contrib[i]) + Float.parseFloat(partials[i]));
+					partials[i] += Float.parseFloat(contrib[i]);
 				}
 			}
 			
-			StringBuilder newWeights = new StringBuilder();
 			for(int i=0;i<DIM;i++){
-				float wi = Float.parseFloat(weights[i])-LAMBDA*Float.parseFloat(partials[i]);
+				float wi = weights[i]-LAMBDA*partials[i];
 				newWeights.append(""+wi+",");
 			}
-
-			out.write(newWeights.toString().getBytes());
 		}
 
 		protected void cleanup(Context context) throws IOException, InterruptedException {
+			weightPath = new Path("s3://finalapp/weight/data.txt");
+			out = fs.create(weightPath);
+			out.write(newWeights.toString().getBytes());
 			out.close();
 		}
 	}
@@ -245,11 +231,11 @@ public class LR_BGD {
 		System.exit(0);
 	}
 
-	public static float logistic(String[] weights, String[] strs) {
-		assert(weights.length==strs.length-1);
+	public static float logistic(float[] weights, float[] xs) {
+		assert(weights.length==xs.length-1);
 		float z = 0;
 		for(int i=0;i<weights.length;i++){
-			z += Float.parseFloat(weights[i])*Float.parseFloat(strs[i]);
+			z += weights[i]*xs[i];
 		}
 		return (float) (1.0/(1+Math.exp(-z)));
 	}
@@ -265,7 +251,7 @@ public class LR_BGD {
 		/*
 		 * Updating weights using Batch GD
 		 */
-		Job jobBGD = new Job(conf, "LR_BGD");
+		Job jobBGD = Job.getInstance(conf, "LR_BGD");
 		jobBGD.setJarByClass(LR_BGD.class);
 		jobBGD.setMapperClass(BGDMapper.class);
 		jobBGD.setReducerClass(BGDReducer.class);
@@ -274,9 +260,6 @@ public class LR_BGD {
 		jobBGD.setOutputValueClass(Text.class);
 		FileInputFormat.addInputPath(jobBGD, new Path(otherArgs[0]));
 		FileOutputFormat.setOutputPath(jobBGD, new Path(otherArgs[2]));
-
-		// Configure the DistributedCache for weights file
-		jobBGD.addCacheFile(new Path(otherArgs[1] + "/data.txt").toUri());
 
 		boolean finished = jobBGD.waitForCompletion(true);
 
@@ -288,14 +271,16 @@ public class LR_BGD {
 			fs.delete(outputFolder, true);
 		}
 
-		if (!finished)
+		if (!finished){			
+			System.err.println("jobBGD aborted!");
 			System.exit(1);
+		}
 
 		/*
 		 * Calculating MSE and decide whether continue the iteration. MSEReduce
 		 * task should sum the SEs and get the mean then update global indicator
 		 */
-		Job jobMSE = new Job(conf, "LR_MSE");
+		Job jobMSE = Job.getInstance(conf, "LR_MSE");
 		jobMSE.setJarByClass(LR_BGD.class);
 		jobMSE.setMapperClass(MSEMapper.class);
 		jobMSE.setReducerClass(MSEReducer.class);
@@ -305,14 +290,14 @@ public class LR_BGD {
 		FileInputFormat.addInputPath(jobMSE, new Path(otherArgs[0]));
 		FileOutputFormat.setOutputPath(jobMSE, new Path(otherArgs[2]));
 
-		// Configure the DistributedCache for weights file
-		jobMSE.addCacheFile(new Path(otherArgs[1] + "/data.txt").toUri());
 		jobMSE.getCounters().findCounter(MSECounter.Terminate).setValue(0);
 
 		finished = jobMSE.waitForCompletion(true);
 
-		if (!finished)
+		if (!finished){			
+			System.err.println("jobMSE aborted!");
 			System.exit(1);
+		}
 
 		// checking the global indicator to decide if continue to next
 		// iteration
